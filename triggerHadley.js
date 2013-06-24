af2738e06485d2b05fdf31e5ecbca584a1e95c17
William McKenna <spacevacation@gmail.com>
2013-06-24 12:14:50 -0400

    testing branch vs master
    perhaps I can't merge on slipring.github.io parent dir?

---
+++ triggerHadley.js
@@ -0,0 +1,71 @@
+
+		Reveal.addEventListener( 'intro', function() {
+			console.log( '"intro" has fired' );
+/*		   		document.getElementById('LtoS').src='flux/JITTER5.gif';*/
+		} );
+
+		Reveal.addEventListener( 'regimes', function() {
+			console.log( '"regimes" has fired' );
+		
+/*			   document.getElementById('hadleyio').src='flux/peartankscape15w.gif';
+			   document.getElementById('worldio').src='flux/pearworldapart15w.gif';
+		   	   document.getElementById('eddio').src='flux/peartankeddy15w.gif';*/
+/*	  		   document.getElementById('LtoS').src='cursors/cloud.png';
+*/		} );
+
+		Reveal.addEventListener( 'pretracers', function() {
+			console.log( '"pretracers" has fired' );
+	  	} );
+
+		Reveal.addEventListener( 'vimvid', function() {
+			console.log( '"vimvid" has fired' );
+	  	} );
+
+		Reveal.addEventListener( 'vlab', function() {
+			console.log( '"vlab" has fired' );
+/*		 	   document.getElementById('emily').src='image/hadley_render2s.gif';*/
+   		} );
+
+		Reveal.addEventListener( 'freecam', function() {
+			console.log( '"freecam" has fired' );
+	  	} );
+
+		Reveal.addEventListener( 'tracerlooks', function() {
+			console.log( '"tracerlooks" has fired' );
+	  	} );
+
+		Reveal.addEventListener( 'greenrelease', function() {
+			console.log( '"greenrelease" has fired' );
+	  	} );
+
+		Reveal.addEventListener( 'pinkrelease', function() {
+			console.log( '"pinkrelease" has fired' );
+	  	} );
+
+		Reveal.addEventListener( 'pinkspread', function() {
+			console.log( '"pinkspread" has fired' );
+	  	} );
+
+		Reveal.addEventListener( 'greenwinding', function() {
+			console.log( '"greenwinding" has fired' );
+	  	} );
+
+	  	Reveal.addEventListener( 'particletrk', function() {
+			console.log( '"particletrk" has fired' );
+	  	} );
+
+	  	Reveal.addEventListener( 'schemata', function() {
+			console.log( '"schemata" has fired' );
+	  	} );
+
+	  	Reveal.addEventListener( 'numnumnum', function() {
+			console.log( '"numnumnum" has fired' );
+	  	} );
+
+	  	Reveal.addEventListener( 'climated', function() {
+			console.log( '"climated" has fired' );
+	  	} );
+
+		/*$('.swap').click(function(){ 
+    		$(this).attr('src','new/path/to/img.jpg');
+		});*/
